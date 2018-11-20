f2c582c7f23cfce4ac3074b55c4e19c6f0db9321
Nick Mages <nickmages@gmail.com>
2018-11-20 14:28:40 -0600

    add Jenkins pipeline for ci/cd

--- src/test/javascript/e2e/entities/customer/customer.spec.ts
+++ src/test/javascript/e2e/entities/customer/customer.spec.ts
@@ -11,7 +11,7 @@ describe('Customer e2e test', () => {
     let signInPage: SignInPage;
     let customerUpdatePage: CustomerUpdatePage;
     let customerComponentsPage: CustomerComponentsPage;
-    /*let customerDeleteDialog: CustomerDeleteDialog;*/
+    let customerDeleteDialog: CustomerDeleteDialog;
 
     before(async () => {
         await browser.get('/');
@@ -34,9 +34,8 @@ describe('Customer e2e test', () => {
         await customerUpdatePage.cancel();
     });
 
-    /* it('should create and save Customers', async () => {
+    it('should create and save Customers', async () => {
         const nbButtonsBeforeCreate = await customerComponentsPage.countDeleteButtons();
-
         await customerComponentsPage.clickOnCreateButton();
         await customerUpdatePage.setFirstNameInput('firstName');
         expect(await customerUpdatePage.getFirstNameInput()).to.eq('firstName');
@@ -60,19 +59,18 @@ describe('Customer e2e test', () => {
         expect(await customerUpdatePage.getSaveButton().isPresent()).to.be.false;
 
         expect(await customerComponentsPage.countDeleteButtons()).to.eq(nbButtonsBeforeCreate + 1);
-    });*/
+    });
 
-    /* it('should delete last Customer', async () => {
+    it('should delete last Customer', async () => {
         const nbButtonsBeforeDelete = await customerComponentsPage.countDeleteButtons();
         await customerComponentsPage.clickOnLastDeleteButton();
 
         customerDeleteDialog = new CustomerDeleteDialog();
-        expect(await customerDeleteDialog.getDialogTitle())
-            .to.eq('storeApp.customer.delete.question');
+        expect(await customerDeleteDialog.getDialogTitle()).to.eq('storeApp.customer.delete.question');
         await customerDeleteDialog.clickOnConfirmButton();
 
         expect(await customerComponentsPage.countDeleteButtons()).to.eq(nbButtonsBeforeDelete - 1);
-    });*/
+    });
 
     after(async () => {
         await navBarPage.autoSignOut();
